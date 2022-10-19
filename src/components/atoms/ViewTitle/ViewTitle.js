25052af68d3e104a9b41905615dd7f03410849fd
Patryk Glinka <patrykglinka2@gmail.com>
2022-10-19 19:39:08 +0200

    Created external components - ViewWrapper and ViewTitle

---
+++ src/components/atoms/ViewTitle/ViewTitle.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const StyledTitle = styled.h1`
+  color: ${({ theme }) => theme.colors.grey};
+  font-size: ${({ theme }) => theme.fontSize.xxl};
+`;
--- src/components/atoms/ViewWrapper/ViewWrapper.js
+++ src/components/atoms/ViewWrapper/ViewWrapper.js
@@ -12,10 +12,3 @@ export const Wrapper = styled.div`
   align-items: center;
   justify-content: center;
 `;
-
-export const StyledList = styled.ul``;
-
-export const StyledTitle = styled.h1`
-  color: ${({ theme }) => theme.colors.grey};
-  font-size: ${({ theme }) => theme.fontSize.xxl};
-`;
