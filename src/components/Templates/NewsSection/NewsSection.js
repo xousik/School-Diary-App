202aa46c6c7d19dae5587fb70b9076f86ee10c7f
Patryk Glinka <patrykglinka2@gmail.com>
2022-10-26 17:36:55 +0200

    Styling News Section component

--- src/components/Templates/NewsSection/NewsSection.js
+++ src/components/Templates/NewsSection/NewsSection.js
@@ -1,27 +1,46 @@
 import React from 'react';
-import { Wrapper } from 'components/Templates/NewsSection/NewsSection.styles';
-import { ViewWrapper } from 'components/moleculs/ViewWrapper/ViewWrapper';
+import { NewsSectionHeader, Wrapper, ArticleWrapper, TitleWrapper, ContentWrapper } from './NewsSection.styles';
 import { Button } from 'components/atoms/Button/Button';
+
+const data = [
+  {
+    title: 'New computers at school',
+    category: 'Tech news',
+    content:
+      'Pariatur ipsam non maiores laboriosam officia recusandae ea unde. Dolores amet non aperiam dolor voluptates at qui. Dolorem sapiente aperiam nostrum. Porro accusantium quia praesentium voluptatibus.',
+    image: null,
+  },
+  {
+    title: 'New computers at school',
+    category: 'Tech news',
+    content: 'Recusandae illo perspiciatis illo ea eligendi est et rerum sdfjkhasd asdjhfkasjdf ajsdfhjkasdhf askdjfhasjkdfh aksjdfh jka.',
+    image: 'https://unsplash.it/400/300',
+  },
+  {
+    title: 'New computers at school',
+    category: 'Tech news',
+    content: 'Consequatur maxime sint corporis rerum eos consequuntur sunt voluptatem.',
+    image: null,
+  },
+];
 
 export const NewsSection = () => {
   return (
     <Wrapper>
-      <h2>News feed section</h2>
-      <ViewWrapper>
-        <h3>Lorem ipsum</h3>
-        <p>dolor sit amet</p>
-        <Button>click me</Button>
-      </ViewWrapper>
-      <ViewWrapper>
-        <h3>Lorem ipsum</h3>
-        <p>dolor sit amet</p>
-        <Button>click me</Button>
-      </ViewWrapper>
-      <ViewWrapper>
-        <h3>Lorem ipsum</h3>
-        <p>dolor sit amet</p>
-        <Button>click me</Button>
-      </ViewWrapper>
+      <NewsSectionHeader>News feed section</NewsSectionHeader>
+      {data.map(({ title, category, content, image = null }) => (
+        <ArticleWrapper key={title}>
+          <TitleWrapper>
+            <h3>{title}</h3>
+            <p>{category}</p>
+          </TitleWrapper>
+          <ContentWrapper>
+            <p>{content}</p>
+            {image ? <img src={image} alt="" /> : null}
+          </ContentWrapper>
+          <Button>click me</Button>
+        </ArticleWrapper>
+      ))}
     </Wrapper>
   );
 };
--- src/components/Templates/NewsSection/NewsSection.styles.js
+++ src/components/Templates/NewsSection/NewsSection.styles.js
@@ -1,7 +1,52 @@
 import styled from 'styled-components';
+import { ViewWrapper } from 'components/moleculs/ViewWrapper/ViewWrapper';
 
 export const Wrapper = styled.div`
   grid-row: 1/3;
   grid-column: 3/3;
   border-left: 1px solid ${({ theme }) => theme.colors.darkPurple};
+  padding: 50px;
+  overflow-y: scroll;
+`;
+
+export const NewsSectionHeader = styled.h3`
+  margin-right: auto;
+  color: ${({ theme }) => theme.colors.darkGrey};
+  font-size: ${({ theme }) => theme.fontSize.xxl};
+`;
+
+export const ArticleWrapper = styled(ViewWrapper)`
+  margin: 30px 0;
+  width: 100%;
+  max-width: unset;
+  border-radius: 12px;
+  color: ${({ theme }) => theme.colors.darkGrey};
+
+  p {
+    line-height: 1.6;
+    margin-top: 15px;
+  }
+`;
+
+export const TitleWrapper = styled.div`
+  h3 {
+    font-size: ${({ theme }) => theme.fontSize.xl};
+  }
+
+  p {
+    margin: 0;
+    font-size: ${({ theme }) => theme.fontSize.l};
+    color: ${({ theme }) => theme.colors.darkGrey};
+  }
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  column-gap: 25px;
+
+  img {
+    max-width: 200px;
+    object-fit: cover;
+    border-radius: 10px;
+  }
 `;
